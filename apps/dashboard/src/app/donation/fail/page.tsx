f8e1d729c65594dd0505ba03fcd18e79fb2d1c30
aung myo myat <dev.server.mm@gmail.com>
2023-07-31 21:18:19 +0630

    chore(dash): add payment failed page

---
+++ apps/dashboard/src/app/donation/fail/page.tsx
@@ -0,0 +1,8 @@
+export default function DonationFailPage() {
+  return (
+    <div className="mt-40 flex flex-col justify-center items-center  w-full h-[30vh]">
+      <h1 className="text-4xl font-bold">Opps</h1>
+      <p className="text-xl">Your payment failed try again</p>
+    </div>
+  );
+}
