9c18e21350853475cd5a81df1f173a6d8cfb6be2
aung myo myat <dev.server.mm@gmail.com>
2023-07-31 12:33:42 +0630

    chore(dash): change thank you message

--- apps/dashboard/src/app/donation/success/page.tsx
+++ apps/dashboard/src/app/donation/success/page.tsx
@@ -1,9 +1,8 @@
-import { ThankYouMessage } from './components/thank-you-message';
-
 export default function DonationSuccessPage() {
   return (
-    <div className="mt-40 flex justify-center items-center  w-full h-[30vh]">
-      <ThankYouMessage />
+    <div className="mt-40 flex flex-col justify-center items-center  w-full h-[30vh]">
+      <h1 className="text-4xl font-bold">Thank You</h1>
+      <p className="text-xl">For supporting me with your donation</p>
     </div>
   );
 }
