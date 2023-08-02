ca6e87cba8ab19de4318dae92298ac59ad042bb6
aung myo myat <dev.server.mm@gmail.com>
2023-08-02 21:58:35 +0630

    chore(api): payment cut fee is now 5 percent

--- apps/api/src/payment/payment.constants.ts
+++ apps/api/src/payment/payment.constants.ts
@@ -1,2 +1,2 @@
-export const PAYMENT_FEE_CUT_FOR_STREAMER = '10'; // percent
+export const PAYMENT_FEE_CUT_FOR_STREAMER = '5'; // percent
 export const FIX_FEE_MUST_BE_NUMBER = 'fix fee must be number';
