d8a46512db23afbf2945280965adaa75ab062896
aung myo myat <dev.server.mm@gmail.com>
2023-08-18 15:47:05 +0630

    chore(dash): enable kbz payment

---
+++ apps/dashboard/src/app/(home)/components/section-one.tsx
@@ -0,0 +1,7 @@
+export function SectionOne() {
+  return (
+    <div>
+      <h1>section one</h1>
+    </div>
+  );
+}
--- apps/dashboard/src/components/home/index.tsx
+++ apps/dashboard/src/components/home/index.tsx
@@ -4,16 +4,12 @@ import { Button } from '../ui/button';
 export default function HomeSectionOne() {
   return (
     <section className="text-gray-600 body-font">
-      <div className="max-w-5xl pt-52 pb-24 mx-auto">
-        <h1 className="text-3xl text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
-          Tip Alerts For Streamer
+      <div className="max-w-5xl pt-30 pb-24 mx-auto">
+        <h1 className="text-6xl text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
+          Accepts Myanmar Payment Methods on Streamlabs
         </h1>
         <h2 className="text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray-700 text-center">
-          StreamerChit is a service for streamers to receive tip alerts.
-          <br />
-          {
-            'The platform is still in beta, so the landing page is not yet complete :('
-          }
+          Payments Integration with Streamlabs
         </h2>
         <div className="ml-6 text-center">
           <Link href={'/sign-in'} passHref>
