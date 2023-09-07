18426e7ee48528b32463f06c8f1386b08e258d45
aung myo myat <dev.server.mm@gmail.com>
2023-09-07 14:01:13 +0630

    clean up

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ConfigModule } from '@nestjs/config';
 import { ENV_SCHEMA } from './env.schema';
@@ -39,8 +37,6 @@ import { StreamlabsModule } from './lib/streamlabs/streamlabs.module';
     PayoutModule,
     StreamlabsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- apps/api/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import EventEmitter from 'events';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
