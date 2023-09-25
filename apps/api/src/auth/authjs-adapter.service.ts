2da5841ff921d1530b00b662f022a63c6ddf1b67
aung myo myat <dev.server.mm@gmail.com>
2023-09-25 19:12:44 +0630

    chore(api): remove unnesscessary transaction

--- apps/api/src/auth/authjs-adapter.service.ts
+++ apps/api/src/auth/authjs-adapter.service.ts
@@ -33,56 +33,49 @@ export class AuthjsAdapterService {
   createUser(data: Prisma.UserCreateInput, invite_to_register_id: string) {
     const page_handle = nanoid(10);
     const user_id = nanoid(20);
-    return this.prisma.$transaction(
-      async (tx) => {
-        // const inviteToRegister = await tx.inviteToRegister.findUniqueOrThrow({
-        //   where: { id: invite_to_register_id },
-        // });
-        // if (inviteToRegister.used_by) {
-        //   throw new TRPCError({
-        //     code: 'FORBIDDEN',
-        //     cause: 'invite code',
-        //     message: 'invite code already used',
-        //   });
-        // }
-        const p2 = tx.user.create({
-          data: {
-            ...data,
-            id: user_id,
-            Balance: { create: {} },
-            file_library: { create: { total_size_in_byte: '0' } },
-            donation_setting: {
-              create: {
-                duration: 10,
-                font_color: '#00FF00',
-                font_size: '48px',
-                font_weight: 800,
-                message_font_color: '#FFFFFF',
-                message_font_weight: 700,
-                image_href:
-                  'https://media.giphy.com/media/VVGdG2HimJl6APwPiE/giphy-downsized.gif',
-                sound_href:
-                  'https://www.redringtones.com/wp-content/uploads/2019/02/wubba-lubba-dub-dub-ringtone.mp3',
-                message_font_size: '24px',
-                alertbox_listener_token:
-                  this.alertboxService.generateJwtToken(user_id),
-              },
-            },
-            donation_page: {
-              create: {
-                url_handle: page_handle,
-                avatar_url: data.image || '',
-                page_cover: '',
-                display_name: data.name || '',
-              },
-            },
+    // const inviteToRegister = await tx.inviteToRegister.findUniqueOrThrow({
+    //   where: { id: invite_to_register_id },
+    // });
+    // if (inviteToRegister.used_by) {
+    //   throw new TRPCError({
+    //     code: 'FORBIDDEN',
+    //     cause: 'invite code',
+    //     message: 'invite code already used',
+    //   });
+    // }
+    const user = this.prisma.user.create({
+      data: {
+        ...data,
+        id: user_id,
+        Balance: { create: {} },
+        file_library: { create: { total_size_in_byte: '0' } },
+        donation_setting: {
+          create: {
+            duration: 10,
+            font_color: '#00FF00',
+            font_size: '48px',
+            font_weight: 800,
+            message_font_color: '#FFFFFF',
+            message_font_weight: 700,
+            image_href:
+              'https://media.tenor.com/EXevW7XQUc4AAAAj/timothy-winchester-littlest-friends.gif',
+            sound_href: 'https://streamlabs.com/sounds/gallery/default.ogg',
+            message_font_size: '24px',
+            alertbox_listener_token:
+              this.alertboxService.generateJwtToken(user_id),
           },
-        });
-        const [user] = await Promise.all([p2]);
-        return user;
+        },
+        donation_page: {
+          create: {
+            url_handle: page_handle,
+            avatar_url: data.image || '',
+            page_cover: '',
+            display_name: data.name || '',
+          },
+        },
       },
-      { maxWait: 10000, timeout: 20000 }
-    );
+    });
+    return user;
   }
   getUser(id: string) {
     return this.prisma.user.findUnique({ where: { id } });
