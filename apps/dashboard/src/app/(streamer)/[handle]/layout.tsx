6607ad5dec01bf580903c3ab954b215c9765b543
aung myo myat <dev.server.mm@gmail.com>
2023-10-27 11:37:51 +0630

    chore(dash):dynamically add favicon for streamer tip page

--- apps/dashboard/src/app/(streamer)/[handle]/layout.tsx
+++ apps/dashboard/src/app/(streamer)/[handle]/layout.tsx
@@ -1,11 +1,27 @@
-import type { Metadata } from 'next';
 import { TipePageProvider } from './providers/tip-page-provider';
 import { client } from '@/lib/trpc';
 export const revalidate = 0;
-export const metadata: Metadata = {
-  title: 'streamer achit',
-  description: 'streamer achit',
+import type { Metadata, ResolvingMetadata } from 'next';
+
+type Props = {
+  params: { handle: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const streamerData = await client.user.info.query({
+    page_handle: params.handle,
+  });
+
+  return {
+    title: streamerData.name,
+    icons: streamerData.image,
+  };
+}
+
 function getStreamerInfo(page_handle: string) {
   return client.user.info.query({ page_handle });
 }
